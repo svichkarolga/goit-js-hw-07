589e5582b659f323a50fda6a9eb5eef8265e3352
svichkar olga <syns41@gmail.com>
2024-07-19 17:44:49 +0300

    task-2 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
  const listCategories = document.querySelector('#categories');
- const items = listCategories.querySelector('.item');
+ const items = listCategories.querySelectorAll('.item');
  console.log(`Кількість категорій: ${items.length}`)
 
-    items.forEach(listItems => {
-    const title = item.querySelector('.h2').textContent;
-    const elCount = item.querySelector('.li').length;
+    items.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const elCount = item.querySelectorAll('li').length;
     console.log(`Category ${title}`);
     console.log(`Elements ${elCount}`);
 });
